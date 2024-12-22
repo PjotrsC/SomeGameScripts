dc67f6c15d22822308ce85b5658e01c284542763
PjotrsC <thugpriority@gmail.com>
2024-12-22 08:48:23 +0200

    MyGame PlayerController added Gizmos

--- Scripts/Player/PlayerController(MyGame).cs
+++ Scripts/Player/PlayerController(MyGame).cs
@@ -25,6 +25,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool ignoreHeight;
     [SerializeField] private Transform aimedTransform;
 
+    [Header("Weapon Holder")]
+    [SerializeField] private WeaponSwitching weaponSwitching;
+
+    [Header("Gizmos")]
+    [SerializeField] private bool gizmo_cameraRay = false;
+    [SerializeField] private bool gizmo_ground = false;
+    [SerializeField] private bool gizmo_target = false;
+    [SerializeField] private bool gizmo_ignoredHeightTarget = false;
+
+
     private void Awake()
     {
    
@@ -190,5 +200,67 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheckPoint.position, 0.25f);
+
+        #region Gimos for checking position of mouse AFTER Delete
+        if (Application.isPlaying == false) return;
+
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hitInfo, float.MaxValue, whatIsGround))
+        {
+            if (gizmo_cameraRay)
+            {
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawLine(ray.origin, hitInfo.point);
+                Gizmos.DrawWireSphere(ray.origin, 0.5f);
+            }
+
+            var hitPosition = hitInfo.point;
+            var hitGroundHeight = Vector3.Scale(hitInfo.point, new Vector3(1, 0, 1)); ;
+            var hitPositionIngoredHeight = new Vector3(hitInfo.point.x, aimedTransform.position.y, hitInfo.point.z);
+
+            if (gizmo_ground)
+            {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawWireSphere(hitGroundHeight, 0.5f);
+                Gizmos.DrawLine(hitGroundHeight, hitPosition);
+            }
+
+            if (gizmo_target)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(hitInfo.point, 0.5f);
+                Gizmos.DrawLine(aimedTransform.position, hitPosition);
+            }
+
+            if (gizmo_ignoredHeightTarget)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireSphere(hitPositionIngoredHeight, 0.5f);
+                Gizmos.DrawLine(aimedTransform.position, hitPositionIngoredHeight);
+            }
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, 0.25f);
+        #endregion 
+    }
+    private void GizmoSettings()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gizmo_cameraRay = !gizmo_cameraRay;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gizmo_ground = !gizmo_ground;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gizmo_target = !gizmo_target;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            gizmo_ignoredHeightTarget = !gizmo_ignoredHeightTarget;
+        }
     }
 }
